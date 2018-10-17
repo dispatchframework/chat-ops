6a6d6ddc3e8ea2f6f93ee8ed715dc96ec55408f4
Karol Stepniewski <kstepniewski@vmware.com>
2018-10-17 15:03:07 -0700

    Enhance ChatOps to work with multiple clouds
    * adds support for AWS, GCP and Azure
    * adds support for `create`, `list`, `delete` commands
    
    Example:
    To create an instance on all clouds at the same time, the command would be:
    `/cloudmaster create myVM on all`
    
    switch `all` to `gcp`, `azure` or `aws` to deploy only to selected
    cloud.

---
+++ aws.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+#######################################################################
+# Copyright (c) 2018 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#######################################################################
+
+import json
+
+import boto3
+
+
+def list_instances(ec2):
+    result = ec2.instances.filter(
+        Filters=[
+            {
+                'Name': 'tag:managedby',
+                'Values': ['cloudmaster']
+            }
+        ]
+    )
+    return [{
+            "id": i.id,
+            "name": [t["Value"] for t in i.tags if t["Key"] == "Name"][0],
+            "state": i.state["Name"],
+            } for i in result if i.state["Name"] != "terminated"]
+
+
+def create_instance(ec2, name):
+    result = ec2.create_instances(
+        MinCount=1, MaxCount=1, ImageId="ami-0f47ef92b4218ec09",
+        InstanceType="t1.micro",
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'managedby',
+                        'Value': 'cloudmaster'
+                    },
+                    {
+                        'Key': 'Name',
+                        'Value': name
+                    }
+                ]
+            }
+        ])
+    instance = result[0]
+    return {
+        "id": instance.id,
+        "name": name,
+    }
+
+
+def delete_instance(ec2_resource, ec2_client, name):
+    instances = ec2_resource.instances.filter(
+        Filters=[
+            {
+                'Name': 'tag:managedby',
+                'Values': ['cloudmaster']
+            },
+            {
+                'Name': "tag:Name",
+                'Values': [name]
+            }
+        ]
+    )
+    instanceIds = [i.id for i in instances]
+    print(instanceIds)
+    return ec2_client.terminate_instances(InstanceIds=instanceIds)
+
+
+def handle(ctx, payload):
+    """
+    entry point for GCP commands
+    """
+
+    access_key = ctx['secrets']['aws']['access_key']
+    secret_key = ctx['secrets']['aws']['secret_key']
+
+    ec2_resource = boto3.resource("ec2", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    ec2_client = boto3.client("ec2", aws_access_key_id=access_key,
+                              aws_secret_access_key=secret_key)
+
+    if 'command' not in payload:
+        return _error('command is required')
+    command = payload['command']
+
+    result = {"error": "command {} is not supported".format(command)}
+    if command == 'create':
+        result = create_instance(ec2_resource, payload['name'])
+    if command == 'list':
+        result = list_instances(ec2_resource)
+    if command == 'delete':
+        result = delete_instance(ec2_resource, ec2_client, payload['name'])
+
+    return json.dumps(result)
+
+
+def _error(error_msg):
+    return json.dumps({
+        'error': error_msg
+    })
+
+
+if __name__ == "__main__":
+    import sys
+
+    with open(sys.argv[1]) as p:
+        payload = json.load(p)
+
+    with open(sys.argv[2]) as s:
+        secrets = json.load(s)
+    ctx = {
+        "secrets": secrets
+    }
+    print("payload: ", payload, file=sys.stderr)
+    print("secrets: ", secrets, file=sys.stderr)
+    retval = handle(ctx, payload)
+    print(retval, file=sys.stdout)
---
+++ vsphere.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+#######################################################################
+# Copyright (c) 2018 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#######################################################################
+
+"""
+Example function to manage (create, delete, list) VMs in Azure.
+
+** REQUIREMENTS **
+
+* secret
+
+you can  obtain clientId, password and tenant when creating a service principal. you can create service principal
+in Azure by using Azure CLI:
+
+az ad sp create-for-rbac --name principalName --password "" # empty password will generate a random one
+
+Store all the required secrets in a JSON file.
+
+cat << EOF > azure.json
+{
+{
+    "clientId": "<UUID>",
+    "password": "<UUID>",
+    "tenant": "<UUID>",
+    "subscription": "<UUID>",
+    "admin_password": "<Password for admin account within a VM>",
+    "location": "<Azure region, e.g. useast2>",
+    "subnet": "<name of the subnet to connect VM to>",
+    "virtual_network": "<name of the virtual network where the subnet resides>"
+  }
+}
+EOF
+
+Create a secret:
+
+dispatch create secret azure azure.json
+
+* image
+
+Create requirements file:
+
+cat << EOF > requirements.txt
+azure==4.0.0
+EOF
+
+dispatch create base-image python3-base dispatchframework/python3-base:0.0.3 --language python3
+dispatch create image python-azure python3-base --runtime-deps requirements.txt
+
+Create a function:
+dispatch create function python-azure azure azurecloud.py --secret azure
+
+Execute it:
+dispatch exec azure --wait --input='{"command": "create","name": "exampleVM"}'
+
+"""
+
+import json
+import ssl
+
+from pyVmomi import vim
+from pyVim.connect import SmartConnect, Disconnect
+
+
+def get_obj(content, vimtype, name):
+    """Return an object by name, if name is None the
+    first found object is returned
+    """
+    obj = None
+    container = content.viewManager.CreateContainerView(
+        content.rootFolder, vimtype, True)
+    for c in container.view:
+        if name:
+            if c.name == name:
+                obj = c
+                break
+        else:
+            obj = c
+            break
+    return obj
+
+
+def list_vms(vm_folder):
+    """Returns a list of instances managed by cloudmaster"""
+
+    return [
+        {
+            'name': vm.summary.config.name,
+            'id': vm.summary.config.instanceUuid,
+        }
+        for vm in vm_folder.childEntity]
+
+
+def create_vm(content, template, vm_name, datacenter_name, vm_folder, resource_pool, power_on):
+    """
+    Clone a VM from a template/VM, datacenter_name, vm_folder, datastore_name
+    cluster_name, resource_pool, and power_on are all optional.
+    """
+
+    # if none get the first one
+    datacenter = get_obj(content, [vim.Datacenter], datacenter_name)
+
+    if vm_folder:
+        destfolder = get_obj(content, [vim.Folder], vm_folder)
+    else:
+        destfolder = datacenter.vmFolder
+
+    resource_pool = get_obj(content, [vim.ResourcePool], resource_pool)
+
+    relospec = vim.vm.RelocateSpec()
+    relospec.datastore = template.datastore[0]
+    relospec.pool = resource_pool
+
+    clonespec = vim.vm.CloneSpec()
+    clonespec.location = relospec
+    clonespec.powerOn = power_on
+
+    print("creating VM...")
+    print("clone spec: %s" % clonespec)
+    task = template.Clone(folder=destfolder, name=vm_name, spec=clonespec)
+    return clonespec, task.info.state
+
+
+def delete_vm(content, name):
+    """Delete an instance"""
+    vm = get_obj(content, [vim.VirtualMachine], name)
+    if vm:
+        vm.Destroy_Task()
+    return {
+        'status': 'vm deletion started'
+    }
+
+
+def _error(error_msg):
+    return json.dumps({
+        'error': error_msg
+    })
+
+
+def handle(ctx, payload):
+    """entrypoint for vSphere operations"""
+
+    template_name = 'dispatch-photon'
+
+    secrets = ctx["secrets"]
+    if secrets is None:
+        raise Exception("Requires vsphere secrets")
+
+    host = secrets.get("host")
+    port = secrets.get("port", 443)
+    dc_name = secrets.get("datacenter")
+    vm_folder = secrets.get("vmFolder")
+    resource_pool = secrets.get("resourcePool")
+    username = secrets.get("username")
+    password = secrets.get("password", "")
+
+    vm_name = payload.get("name")
+    power_on = payload.get("powerOn", False)
+
+
+    if 'command' not in payload:
+        return _error('command is required')
+    command = payload['command']
+
+    # default result is unsupported command
+    result = {"error": "command {} is not supported".format(command)}
+
+    context = None
+    if hasattr(ssl, '_create_unverified_context'):
+        context = ssl._create_unverified_context()
+    si = SmartConnect(
+        host=host,
+        user=username,
+        pwd=password,
+        port=port,
+        sslContext=context)
+
+    content = si.RetrieveContent()
+
+    if command == 'create':
+        template = get_obj(content, [vim.VirtualMachine], template_name)
+        if template is None:
+            _error("template not found")
+            return
+        _, result = create_vm(content, template, vm_name, dc_name, vm_folder, resource_pool, power_on)
+
+    if command == 'list':
+        folder = get_obj(content, [vim.Folder], vm_folder)
+        result = list_vms(folder)
+
+    if command == 'delete':
+        result = delete_vm(content, vm_name)
+
+    Disconnect(si)
+
+    return json.dumps(result)
+
+
+if __name__ == "__main__":
+    import sys
+
+    with open(sys.argv[1]) as p:
+        payload = json.load(p)
+
+    with open(sys.argv[2]) as s:
+        secrets = json.load(s)
+    ctx = {
+        "secrets": secrets
+    }
+    print("payload: ", payload, file=sys.stderr)
+    print("secrets: ", secrets, file=sys.stderr)
+    retval = handle(ctx, payload)
+    print(retval, file=sys.stdout)
